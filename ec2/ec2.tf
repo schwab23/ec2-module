372afa85c7da33ac234ce75d3382d593ba735bb7
schwab23 <ebindasi@gmail.com>
2024-03-30 21:11:46 -0500

    module

---
+++ ec2/ec2.tf
@@ -0,0 +1,8 @@
+module "ec2-server" {
+  source        = "../"
+  ami           = "ami-000c0df09737657b6"
+  region_name   = "us-east-2"
+  profile_name  = "default"
+  instance_type = "t3.micro"
+
+}
